b8f66df955debcfd3e30f0f2c6f669d0a6b1ae9e
Murilll <murilosertorio25@gmail.com>
2022-12-16 11:38:16 -0300

    ._.

--- HTML/Site/Portifólio/script.js
+++ HTML/Site/Portifólio/script.js
@@ -1,5 +1,6 @@
 
 function nav(){
     document.getElementById("nav").style.height = "8vh";
+    document.getElementById("body").style.overflow = "scroll";
     // document.getElementById("nav").style.transition = "1s";
 }
